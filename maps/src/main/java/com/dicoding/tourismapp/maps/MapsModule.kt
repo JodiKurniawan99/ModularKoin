7ba82d45acbfd82defec8619c145f26592acca68
Jodi Kurniawan <jodikr8s@gmail.com>
2023-09-24 19:44:00 +0700

    Add dynamic feature maps

---
+++ maps/src/main/java/com/dicoding/tourismapp/maps/MapsModule.kt
@@ -0,0 +1,8 @@
+import com.dicoding.tourismapp.maps.MapsViewModel
+import org.koin.android.viewmodel.compat.ScopeCompat.viewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val mapsModule = module {
+    viewModel { MapsViewModel(get()) }
+}
